af17e2e24fc57fd3781e6885635956e82a07cfa2
Fady Ramzy <fady.ramzy@areebgroup.com>
2021-04-06 14:02:28 +0200

    - Added collection view in home scene viewcontroller xib - Added collection view delegate & datasource methods in home scene viewcontroller file - Added rendring data from presenter on the collection view

--- The Hitchhiker Prophecy/Modules/Home/Home Scene/Views/HomeSceneViewController.swift
+++ The Hitchhiker Prophecy/Modules/Home/Home Scene/Views/HomeSceneViewController.swift
@@ -15,19 +15,69 @@ class HomeSceneViewController: UIViewController {
     var interactor: HomeSceneBusinessLogic?
     var router: HomeSceneRoutingLogic?
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    // MARK: - Properties
+    
+    var charactersViewModel: [HomeScene.Search.ViewModel]?
+    
     // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         interactor?.fetchCharacters()
+        setupCharactersCollectionView()
     }
+    
+    // MARK: - Methods
+    
+    private func setupCharactersCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "HomeCharacterCollectionViewCell", bundle: Bundle(for: HomeCharacterCollectionViewCell.self)), forCellWithReuseIdentifier: "HomeCharacterCollectionViewCell")
 }
+
+}
+
+// MARK: - extensions
 
 extension HomeSceneViewController: HomeSceneDisplayView {
     func didFetchCharacters(viewModel: [HomeScene.Search.ViewModel]) {
-        // TODO: Implement
+        self.charactersViewModel = viewModel
+        collectionView.reloadData()
     }
     
     func failedToFetchCharacters(error: Error) {
         // TODO: Implement
+    }
+}
+
+extension HomeSceneViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension HomeSceneViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: HomeCharacterCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCharacterCollectionViewCell", for: indexPath) as! HomeCharacterCollectionViewCell
+        cell.configure(with: charactersViewModel![indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return charactersViewModel?.count ?? 0
+    }
+}
+
+extension HomeSceneViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.size.width, height: view.frame.size.height)
     }
 }
