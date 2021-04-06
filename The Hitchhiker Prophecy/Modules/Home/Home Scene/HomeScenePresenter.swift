6ee5717d1f3d9ab19a19e46aadb431a5298cbcd6
Fady Ramzy <fady.ramzy@areebgroup.com>
2021-04-06 13:54:37 +0200

    - Added helper method for mapping the character response to view model - Added sucess and failure cases for present character method

--- The Hitchhiker Prophecy/Modules/Home/Home Scene/HomeScenePresenter.swift
+++ The Hitchhiker Prophecy/Modules/Home/Home Scene/HomeScenePresenter.swift
@@ -8,14 +8,38 @@
 
 import Foundation
 
-class HomeScenePresneter: HomeScenePresentationLogic {
+class HomeScenePresneter {
+    
+    // MARK: - Dependencies
+    
     weak var displayView: HomeSceneDisplayView?
+    
+    // MARK: - Initializers
     
     init(displayView: HomeSceneDisplayView) {
         self.displayView = displayView
     }
+}
+
+// MARK: - extensions
+
+extension HomeScenePresneter: HomeScenePresentationLogic {
+    
+    // MARK: - Methods
+    
+    private func mapCharatersOutputToViewModel(with output: Characters.Search.Output) -> [HomeScene.Search.ViewModel] {
+        
+        return output.data.results.map { characterOutput -> HomeScene.Search.ViewModel in
+            return HomeScene.Search.ViewModel(name: characterOutput.name, desc: characterOutput.resultDescription, imageUrl: "\(characterOutput.thumbnail.path).\(characterOutput.thumbnail.thumbnailExtension)", comics: characterOutput.comics.collectionURI, series: characterOutput.series.collectionURI, stories: characterOutput.stories.collectionURI, events: characterOutput.events.collectionURI)
+        }
+    }
     
     func presentCharacters(_ response: HomeScene.Search.Response) {
-        // TODO: Implement
+        switch response {
+        case .success(let output):
+            self.displayView?.didFetchCharacters(viewModel: self.mapCharatersOutputToViewModel(with: output))
+        case .failure(let error):
+            self.displayView?.failedToFetchCharacters(error: error)
+        }
     }
 }
