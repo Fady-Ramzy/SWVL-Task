0f9ee21194bacc15ca26b8093cb72601a8bfcebf
Fady Ramzy <fady.ramzy@areebgroup.com>
2021-04-07 13:18:03 +0200

    - Added unit testing files for Home screen (Home Scene Interactor + Home Presenter Mock Test + Home Scene Spy View) files

---
+++ The Hitchhiker ProphecyTests/Home/Interactor/HomeSceneInteractorMock.swift
@@ -0,0 +1,9 @@
+//
+//  HomeSceneInteractorMock.swift
+//  The Hitchhiker ProphecyTests
+//
+//  Created by Fady Ramzy on 4/6/21.
+//  Copyright © 2021 SWVL. All rights reserved.
+//
+
+import Foundation
---
+++ The Hitchhiker ProphecyTests/Home/View/HomeSceneSpyView.swift
@@ -0,0 +1,9 @@
+//
+//  HomeSceneSpyView.swift
+//  The Hitchhiker ProphecyTests
+//
+//  Created by Fady Ramzy on 4/6/21.
+//  Copyright © 2021 SWVL. All rights reserved.
+//
+
+import Foundation
