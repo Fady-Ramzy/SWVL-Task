10f15a448ae989c5e99d70f66fd968fb5bd37507
Fady Ramzy <fady.ramzy@areebgroup.com>
2021-04-07 18:51:23 +0200

    Removed interactor unit test classes

--- The Hitchhiker ProphecyTests/Home/Interactor/HomeSceneInteractorMock.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HomeSceneInteractorMock.swift
-//  The Hitchhiker ProphecyTests
-//
-//  Created by Fady Ramzy on 4/6/21.
-//  Copyright © 2021 SWVL. All rights reserved.
-//
-
-import Foundation
