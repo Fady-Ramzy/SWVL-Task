203071f3ad00766b74897361bd1b2f118789596c
Fady Ramzy <fady.ramzy@areebgroup.com>
2021-04-07 16:06:28 +0200

    - Implemented character details scene display logic protocol methods in character details mock view - Added unit test methods for character details presenter in character details presenter test -

--- The Hitchhiker ProphecyTests/Character Details/Presenter/CharacterDetailsPresenterTest.swift
+++ The Hitchhiker ProphecyTests/Character Details/Presenter/CharacterDetailsPresenterTest.swift
@@ -7,27 +7,54 @@
 //
 
 import XCTest
+import The_Hitchhiker_Prophecy
 
 class CharacterDetailsPresenterTest: XCTestCase {
 
+    // MARK: - Properties
+    
+    var sut: CharacterDetailsScenePresenter!
+    var view: CharacterDetailsMockView!
+    var response: CharacterDetailsScene.FetchCharacter.Response!
+    
+    // MARK: - View life cycle
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+       response = CharacterDetailsScene.FetchCharacter.Response(id: 1, name: "Character One", resultDescription: "Character Description", modified: "Yes modified", thumbnail: Characters.Search.Character.Thumbnail(path: "Path", thumbnailExtension: ".jpg"), resourceURI: "URI", comics: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), series: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), stories: Characters.Search.Character.Stories(available: 10, collectionURI: "URI", items: [], returned: 10), events: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), urls: [])
+        view = CharacterDetailsMockView()
+        sut = CharacterDetailsScenePresenter(displayView: view)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        view = nil
+        sut = nil
+        response = nil
     }
+    
+    // MARK: - Methods
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_presentCharacter_whenSpecificCharacterIsSelected_shouldCallViewDidFetchCharacter() {
+        
+        // Given
+        // When
+        
+        sut.presentCharacter(response)
+        
+        // Then
+        
+        XCTAssertTrue(view.didFetchCharacter)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    
+    func test_presentCharacter_whenSpecificCharacterIsSelectedWithData_viewModelShouldNotBeNil() {
+        
+        // Given
+        // When
+        
+        sut.presentCharacter(response)
+        
+        // Then
+        
+        XCTAssertNotNil(view.viewModel)
     }
-
 }
--- The Hitchhiker ProphecyTests/Character Details/View/CharacterDetailsMockView.swift
+++ The Hitchhiker ProphecyTests/Character Details/View/CharacterDetailsMockView.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import The_Hitchhiker_Prophecy
+
+class CharacterDetailsMockView: CharacterDetailsSceneDisplayLogic {
+    
+    // MARK: - Properties
+    
+    var didFetchCharacter: Bool = false
+    var viewModel: CharacterDetailsScene.FetchCharacter.ViewModel!
+    
+    // MARK: - Dependencies
+    
+    var interactor: CharacterDetailsSceneBusinessLogic?
+    
+    // MARK: - Methods
+    
+    func didFetchCharacter(viewModel: CharacterDetailsScene.FetchCharacter.ViewModel) {
+        didFetchCharacter = true
+        self.viewModel = viewModel
+    }
+}
