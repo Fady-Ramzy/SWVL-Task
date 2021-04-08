281f80bc7a4b1946cf97da8ca63a0fbdd89923a8
Fady Ramzy <fady.ramzy@areebgroup.com>
2021-04-08 15:19:22 +0200

    - Updated test methods in home scene presenter test class

--- The Hitchhiker ProphecyTests/Home/Presenter/Test/HomePresenterMockTest.swift
+++ The Hitchhiker ProphecyTests/Home/Presenter/Test/HomePresenterMockTest.swift
@@ -15,8 +15,6 @@ class HomePresenterMockTest: XCTestCase {
     
     var sut: HomeScenePresneter!
     var view: HomeSceneMockView!
-    var results: Characters.Search.Results!
-    var output: Characters.Search.Output!
     var characterOne: Characters.Search.Character!
     var characterTwo: Characters.Search.Character!
     
@@ -28,44 +26,16 @@ class HomePresenterMockTest: XCTestCase {
         characterOne = Characters.Search.Character(id: 1, name: "Character One", resultDescription: "Character Description", modified: "Yes modified", thumbnail: Characters.Search.Character.Thumbnail(path: "Path", thumbnailExtension: ".jpg"), resourceURI: "URI", comics: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), series: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), stories: Characters.Search.Character.Stories(available: 10, collectionURI: "URI", items: [], returned: 10), events: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), urls: [])
         
         characterTwo = Characters.Search.Character(id: 1, name: "Character One", resultDescription: "Character Description", modified: "Yes modified", thumbnail: Characters.Search.Character.Thumbnail(path: "Path", thumbnailExtension: ".jpg"), resourceURI: "URI", comics: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), series: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), stories: Characters.Search.Character.Stories(available: 10, collectionURI: "URI", items: [], returned: 10), events: Characters.Search.Character.Comics(available: 1, collectionURI: "URI", items: [], returned: 10), urls: [])
-        
-        
-        results = Characters.Search.Results(offset: 10, limit: 10, total: 10, count: 10, results: [characterOne, characterTwo])
-        output = Characters.Search.Output(code: 10, status: "hhjbv", copyright: "", attributionText: "", attributionHTML: "", etag: "", data: results)
     }
     
     override func tearDownWithError() throws {
         view = nil
         sut = nil
-        results = nil
-        output = nil
         characterOne = nil
         characterTwo = nil
     }
     
     // MARK: - Methods
-    
-    func test_presentCharacters_whenReturnedResponseIsSuccess_shouldCallDidFetchCharacters() {
-        // Given
-        // When
-        
-        sut.presentCharacters(.success(output))
-        
-        // Then
-        
-        XCTAssertTrue(view.didFetchCharacters)
-    }
-    
-    func test_presentCharacters_whenReturnedResponseIsFailure_shouldCallFailedToFetchCharacters() {
-        // Given
-        // When
-        
-        sut.presentCharacters(.failure(.server))
-        
-        // Then
-        
-        XCTAssertTrue(view.didReturnError)
-    }
     
     func test_changePresentationLayout_whenPresentationStyleIsHorizontal_shouldChangePresentationStyleToVertical() {
         // Given
@@ -126,27 +96,58 @@ class HomePresenterMockTest: XCTestCase {
         XCTAssertTrue(view.isCollectionViewPresentedVertically)
     }
     
-    func test_mapCharatersOutputToViewModel_whenDataReturnedWithSuccess_shouldReturn() {
+    func test_mapCharatersOutputToViewModel_whenDataReturnedWithSuccess_shouldReturnMappedCharacters() {
         // Given
         // When
-        let charactersViewModels = sut.mapCharatersOutputToViewModel(with: output)
+        
+        let charactersViewModels = sut.mapCharatersOutputToViewModel(with: [characterOne, characterTwo])
         
         // Then
         
         XCTAssertEqual(charactersViewModels.count, 2)
     }
     
-    func test_mapCharatersOutputToViewModel_whenDataReturnedWithSuccess_shouldDisplayReturnedCharacters() {
+    func test_presentCharacters_WhenDataIsReturnedWithSuccess_shouldCallDidFetchCharacters() {
         // Given
-        
-        sut.presentCharacters(.success(output))
-            
         // When
         
-        _ = sut.mapCharatersOutputToViewModel(with: output)
+        sut.presentCharacters([characterOne, characterOne])
         
         // Then
         
-        XCTAssertEqual(view.numberOfReturnedCharacters, 2)
+        XCTAssertTrue(view.didFetchCharacters)
+    }
+ 
+    func test_handleError_whenAPIReturnsAnError_shouldCallFailedToFetchCharactersWithCannotParseErrorDescription() {
+        // Given
+        // When
+        
+        sut.handleError(with: .cannotParseResponse)
+        
+        // Then
+        
+        XCTAssertFalse(view.returnedError.localizedDescription.isEmpty)
+    }
+    
+    func test_handleError_whenAPIReturnsAnError_shouldCallFailedToFetchCharactersWithServerErrorDescription() {
+        // Given
+        // When
+        
+        sut.handleError(with: .server)
+        
+        // Then
+        
+        XCTAssertFalse(view.returnedError.localizedDescription.isEmpty)
+    }
+    
+    func test_handleError_whenAPIReturnsAnError_shouldCallFailedToFetchCharactersWithUnknownErrorDescription() {
+        // Given
+        // When
+        
+        sut.handleError(with: .unknown)
+        
+        // Then
+        
+        XCTAssertFalse(view.returnedError.localizedDescription.isEmpty)
     }
 }
